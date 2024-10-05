758928866d91ad5a9927a759317619115cf65ddd
Hoang <doanhoang1024@gmail.com>
2024-10-05 11:23:28 +0700

    first commit

---
+++ .vscode/ecommerce website/assets/js/main.js
@@ -0,0 +1,45 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== Menu Show =====*/
+/* Validate if constant exists */
+
+/*===== Hide Show =====*/
+/* Validate if constant exists */
+
+/*=============== IMAGE GALLERY ===============*/
+
+/*=============== SWIPER CATEGORIES ===============*/
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+/*=============== PRODUCTS TABS ===============*/
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll('.tab__btn'),
+          tabContents = document.querySelectorAll('.tab__item');
+
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', () => {
+            // Get the target content based on the data attribute of the clicked tab
+            const targetSelector = tab.dataset.target;
+            const target = document.querySelector(targetSelector);
+
+            // Remove active class from all tab contents
+            tabContents.forEach((tabContent) => {
+                tabContent.classList.remove('active-tab');
+            });
+
+            // Add active class to the clicked tab content
+            if (target) {
+                target.classList.add('active-tab');
+            }
+
+            // Remove active class from all tabs
+            tabs.forEach((tab) => {
+                tab.classList.remove('active-tab');
+            });
+
+            // Add active class to the clicked tab
+            tab.classList.add('active-tab');   
+        });
+    });
+});
